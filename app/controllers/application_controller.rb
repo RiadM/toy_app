813b40137a962e919ee0d93024448b061923e0b1
Riad Mahdid <r.mahdid13+git@gmail.com>
2017-05-10 17:39:33 -0400

    ajouter hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+  	render html: "Salut le monde!"
+  end
+
+  def bye
+  	render html: "Bonne soirée!"
+  end
 end
